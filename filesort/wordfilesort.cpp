56b65be1e5afbb79f8db8fbe04320832500c12e8
Justin LeCheminant <jlechem@gmail.com>
2024-03-02 17:24:12 -0700

    Implementing wordfilesort load method.

--- filesort/wordfilesort.cpp
+++ filesort/wordfilesort.cpp
@@ -2,5 +2,17 @@
 
 void WordFileSort::load(std::string_view fileName)
 {
+	std::ifstream file(fileName.data(), std::ios::in);
 
+	if (file.good())
+	{
+		std::string item;
+
+		while (file >> item)
+		{
+			this->items.push_back(item);
+		}
+
+		file.close();
+	}
 }
